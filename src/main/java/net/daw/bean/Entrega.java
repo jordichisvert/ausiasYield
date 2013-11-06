c5e83e854d50c6d3d54ad6b002dcf0361d44a912
mati <mati@s09-pc03>
2013-11-06 12:45:25 +0100

    pojo entrega de dani

---
+++ src/main/java/net/daw/bean/Entrega.java
@@ -0,0 +1,78 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.daw.bean;
+
+import java.util.Date;
+
+
+
+/**
+ *
+ * @author mati
+ */
+
+public class Entrega {
+   
+  
+    private Integer id;
+
+    private Integer idDocumento;
+
+    private Integer idActividad;
+
+    private Integer nota;
+
+    private Date fecha;
+
+    public Entrega() {
+    }
+
+    public Entrega(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getIdDocumento() {
+        return idDocumento;
+    }
+
+    public void setIdDocumento(Integer idDocumento) {
+        this.idDocumento = idDocumento;
+    }
+
+    public Integer getIdActividad() {
+        return idActividad;
+    }
+
+    public void setIdActividad(Integer idActividad) {
+        this.idActividad = idActividad;
+    }
+
+    public Integer getNota() {
+        return nota;
+    }
+
+    public void setNota(Integer nota) {
+        this.nota = nota;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+   
+    
+}
