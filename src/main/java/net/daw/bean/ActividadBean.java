d3d2e0f4bb1ca74cdba58f44ff81e3239268daa9
mati <mati@s09-pc03>
2013-11-06 12:39:35 +0100

    borrado profesor subida pojo actividad

---
+++ src/main/java/net/daw/bean/ActividadBean.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.daw.bean;
+
+import java.util.Date;
+
+/**
+ *
+ * @author mati
+ */
+public class ActividadBean {
+
+    private Integer id;
+    private String enunciado;
+    private Date fecha;
+    private Integer evaluacion;
+    private Boolean activo;
+
+    public ActividadBean() {
+    }
+
+    public ActividadBean(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEnunciado() {
+        return enunciado;
+    }
+
+    public void setEnunciado(String enunciado) {
+        this.enunciado = enunciado;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Integer getEvaluacion() {
+        return evaluacion;
+    }
+
+    public void setEvaluacion(Integer evaluacion) {
+        this.evaluacion = evaluacion;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+}
