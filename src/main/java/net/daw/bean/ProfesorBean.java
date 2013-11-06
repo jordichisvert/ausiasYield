3709d0c86337f87aaf58953841380d2b4c24de35
mati <mati@s09-pc03>
2013-11-06 12:00:21 +0100

    Creacion del pojo profesor Corregido

--- src/main/java/net/daw/bean/ProfesorBean.java
+++ src/main/java/net/daw/bean/ProfesorBean.java
@@ -99,28 +99,4 @@ public class ProfesorBean {
         this.dni = dni;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof ProfesorBean)) {
-            return false;
-        }
-        ProfesorBean other = (ProfesorBean) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "net.daw.bean.Profesor[ id=" + id + " ]";
-    }
 }
