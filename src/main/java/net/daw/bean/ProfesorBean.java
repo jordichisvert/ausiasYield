985f3b05e1766cd76214cca3ce575dfc3dcd1c1d
mati <mati@s09-pc03>
2013-11-06 11:58:19 +0100

    Creacion del pojo profesor

---
+++ src/main/java/net/daw/bean/ProfesorBean.java
@@ -0,0 +1,126 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.daw.bean;
+
+/**
+ *
+ * @author mati
+ */
+public class ProfesorBean {
+
+    private Integer id;
+    private Integer idUsuario;
+    private String nombre;
+    private String ape1;
+    private String ape2;
+    private String sexo;
+    private String telefono;
+    private String email;
+    private String dni;
+
+    public ProfesorBean() {
+    }
+
+    public ProfesorBean(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApe1() {
+        return ape1;
+    }
+
+    public void setApe1(String ape1) {
+        this.ape1 = ape1;
+    }
+
+    public String getApe2() {
+        return ape2;
+    }
+
+    public void setApe2(String ape2) {
+        this.ape2 = ape2;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ProfesorBean)) {
+            return false;
+        }
+        ProfesorBean other = (ProfesorBean) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "net.daw.bean.Profesor[ id=" + id + " ]";
+    }
+}
